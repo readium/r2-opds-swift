e28f7fa990c62d7b86c4a1c258cd4acbfab694e3
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2021-06-05 10:59:16 -0500

    Temporarily point to forked shared

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/cezheng/Fuzi.git", .exact("3.1.3")),
-        .package(name: "R2Shared", path: "../r2-shared-swift")
+        .package(name: "R2Shared", url: "https://github.com/stevenzeck/r2-shared-swift.git", .branch("use-spm"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
